cb2b9a7702b9e5ee50cd21325776994b2a7d08b7
parthshukla <parthshukla808@gmail.com>
2024-11-08 20:36:30 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode:"class", //requied consfiguration to enable the functionality of selecting from html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
